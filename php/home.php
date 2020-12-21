7c7a77a8a76e860acab1a3c49f25435c5d77283f
gehadkassap <gohadkasaa@gmail.com>
2020-12-21 20:04:35 +0200

    create files

---
+++ php/home.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cafe</title>
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+<section class="container-fluid mt-5">
+    <div class="row">
+        <!------for choice of user------->
+        <div class="col-md-5">
+
+        </div>
+        <!-------------------------->
+         <!------for display to user------->
+         <div class="col-md-7">
+        <h2 class="pb-2" >your Favourite Drinks </h2>
+             <div class="favDrinks">
+                 <div class="row">
+                     <div class="col-md-4">
+                         <div class="card" >
+                            <img src="../imgs/joe-hepburn-EcWFOYOpkpY-unsplash.jpg" alt="" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text"> Lorem ipsum dolor sit amet consectetur.</p>
+                                 <a href="#" class="btn btn-primary">List Details</a>
+                            </div>
+
+
+                         </div>
+                         
+                     </div>
+                     <div class="col-md-4">
+                         <div class="card">
+                            <img src="../imgs/joe-hepburn-EcWFOYOpkpY-unsplash.jpg" alt="" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text"> Lorem ipsum dolor sit amet consectetur.</p>
+                                 <a href="#" class="btn btn-primary">List Details</a>
+                            </div>
+
+
+                         </div>
+                         
+                     </div>
+               
+                 </div>
+             </div>
+         </div>
+        
+
+
+       
+    </div>
+</section>
+
+<script src="../js/jquery-3.5.0.js"></script>
+<script src="../js/popper.min.js"></script>
+
+<script  src="../js/bootstrap.min.js"></script>
+<script src="../js/login.js"></script>
+</body>
+</html>
---
+++ php/index.php
@@ -0,0 +1,44 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="../css/all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <section  id="singIn">
+<div id="form">
+        <div class="container">
+        <p class="text-center "> welcome to our..</p>
+        <h1 class="text-center mb-5">Cafeteria</h1>
+        <form action="home.php" method="POST"  class="w-100 m-auto">
+            <div class="form-group text-center">
+            <input type="text" class=" form-control" placeholder="enter your username">
+                
+            </div>
+            <div class="form-group text-center">
+                <input type="password" class="form-control" placeholder="enter your password">
+                
+            </div>
+        <button class="btn btn-primary w-100">Login </button>
+        </form>
+        </div>
+</div>
+       
+    </section>
+
+
+<script src="../js/jquery-3.5.0.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/jquery.particleground.min.js"></script>
+
+<script  src="../js/bootstrap.min.js"></script>
+<script src="../js/login.js"></script>
+   
+</body>
+</html>
