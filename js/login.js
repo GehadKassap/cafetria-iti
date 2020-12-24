f9a0fe4d1085b140e5c641a93322261e41657966
gehadkassap <gohadkasaa@gmail.com>
2020-12-24 15:19:13 +0200

    login function

--- js/login.js
+++ js/login.js
@@ -1,14 +1,7 @@
 jQuery(document).ready(function(){
     jQuery('#singIn').particleground();
-    jQuery("button").click(function(){
-        if(jQuery(".userName").val() == null)
-           {
-            alert("please enter ur username")
-        } 
-
-    });
-
-
+  
+    
 
 
 
